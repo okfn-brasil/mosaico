f403f8e0ece884d915db8678ee6826ffb96c7501
Vitor Baptista <vitor@vitorbaptista.com>
2013-09-17 12:10:43 -0300

    Change Gruntfile so we can keep the karma tests running on background

--- Gruntfile.js
+++ Gruntfile.js
@@ -292,7 +292,7 @@ module.exports = function (grunt) {
     karma: {
       unit: {
         configFile: 'karma.conf.js',
-        singleRun: true
+        singleRun: false
       }
     },
     cdnify: {
